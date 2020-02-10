7a1da38be99fa6eddbf585b77ff6611d3270097d
Ravi <ruibrado@knox.edu>
2020-02-10 12:05:35 -0600

    2/10/2020

---
+++ CS292-Template/Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+	{
+		Rigidbody2D rb;
+		float speed = 5.0f;
+private Vector2 originalPosition;
+public Joystick joystick;
+
+void Start(){
+rb= GetComponent<Rigidbody2D>();
+originalPosition = transform.localPosition;
+
+
+}
+
+void Update()
+{
+float horizontal = joystick.Horizontal;
+float vertical= joystick.Vertical;
+Vector2 position =rb.position;
+	
+		position.x = position.x + speed * horizontal * Time.deltaTime;
+
+	if (position.y > -5 && position.y < 5){
+		position.y = position.y + speed * vertical * Time.deltaTime;
+			}
+
+rb.MovePosition(position);
+CheckCollisions();
+}
+
+private void CheckCollisions(){
+		GameObject[] gameObjects = GameObject.FindGameObjectsWithTag("Collide");
+
+		foreach (GameObject go in gameObjects){
+			CollidableObject collidableObject = go.GetComponent<CollidableObject>();
+
+			if (collidableObject.IsColliding(this.gameObject)){
+				if (collidableObject.isSafe){
+					
+				} else {
+					transform.localPosition = originalPosition;
+					//transform.GetComponent<SpriteRenderer>().sprite = playerUp;
+				}	
+
+			}
+		}
+	}
+}
